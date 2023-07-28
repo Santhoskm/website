2b04d86ca43b906410602ef388448ce21bbb7864
Santhoskm <113929717+Santhoskm@users.noreply.github.com>
2023-07-28 11:28:12 +0530

    Add files via upload

---
+++ src/commponents/navbar/Navbar.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import './navbar.scss';
+import { RiMenu3Line, RixloseLin } from 'react-icons/ri';
+import logo from "../../assets/logo.png"
+
+
+const Navbar = () => {
+    return (
+        <div className="Ecommers__navbar">
+            <div className="Ecommers__navbar-links">
+                <div className="Ecommers__navbar-links_logo">
+                    <img src={logo} alt="logo" /></div>
+                <div className='Ecommers__navbar-links_containers'>
+                    <p><a herf="#home">Home</a></p>
+                    <p><a herf="#web">What is Web</a></p>
+                    <p><a herf="#features">Case Studies</a></p>
+                    <p><a herf="#library">Library</a></p>
+                </div>
+
+
+            </div>
+            <div className='Ecommers__navbar-sign'>
+                <p>Sign in</p>
+                <button type="button">Sign up</button>
+            </div>
+
+        </div>
+    )
+}
+
+export default Navbar
