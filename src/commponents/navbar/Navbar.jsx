41b08ed228359c8be3e8887bbe79c6cc049574b0
Santhoskm <113929717+Santhoskm@users.noreply.github.com>
2023-08-15 08:48:58 +0530

    Update Navbar.jsx

--- src/commponents/navbar/Navbar.jsx
+++ src/commponents/navbar/Navbar.jsx
@@ -1,12 +1,13 @@
-import React from 'react'
+import React { useState } from 'react'
 import './navbar.scss';
-//import { RiMenu3Line, RixloseLin } from 'react-icons/ri';
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
 import lo from "../../assets/lo.png"
 
 
 
 
 const Navbar = () => {
+    const [toggleMenu, settoggleMenu] = useState(false);
     return (
         <div className="Ecommers__navbar">
             <div className="Ecommers__navbar-links">
@@ -24,7 +25,26 @@ const Navbar = () => {
                 <p>Sign in</p>
                 <button type="button">Sign up</button>
             </div>
+            <div className='Ecommers__navbar-menu'>
+                {toggleMenu
+                    ? <RiCloseLine color='#fff' size={27} onClick={() => settoggleMenu(false)} />
+                    : <RiMenu3Line color='#fff' size={27} onClick={() => settoggleMenu(true)} />
+                }
+                {toggleMenu && (
+                    <div className='Ecommers__navbar-menu_containers scale-up-center'>
+                        <div className='Ecommers__navbar-menu_containers-links'>
 
+                            <p><a href="#/">Home</a></p>
+                            <p><a href="#/about">About web</a></p>
+                            <p><a href="#/library">Library</a></p>
+                            <div className='Ecommers__navbar-menu_containers-links-sign'>
+                                <button type="button">Sign up</button>
+                            </div>
+                        </div>
+                    </div>
+
+                )}
+            </div>
         </div>
     )
 }
